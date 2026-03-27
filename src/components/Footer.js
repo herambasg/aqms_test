b9f34ad4e673e6040ba026c68dc44c5a50ba3ea7
herambasg <herambasg003@gmail.com>
2026-03-28 03:05:03 +0530

    latest code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      <p>Designed and developed by the Class of 2026 HITAM ECE students • © 2025</p>
+      <p>Designed and developed by the Class of 2026 HITAM ECE students © 2025</p>
     </footer>
   );
 }
--- src/components/MainAqiCard.js
+++ src/components/MainAqiCard.js
@@ -3,7 +3,9 @@ import React from 'react';
 function MainAqiCard({ aqi, category }) {
   return (
     <section className="main-aqi-card">
-      <h2>HITAM Air Quality Index</h2>
+      <h2 style={{fontSize: '1.5rem', marginBottom: '2rem', textAlign: 'center', color: '#000000'}}>
+        HITAM Air Quality Index (Live)
+      </h2>
       <p id="calculated-aqi">{aqi}</p>
       <p id="aqi-category">{category}</p>
     </section>
